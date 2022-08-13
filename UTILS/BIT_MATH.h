11a5187969272aa0189c5aa071b42c19d609b365
Mustapha Ossama <109214165+MuOssama@users.noreply.github.com>
2022-08-13 23:46:33 +0200

    Add files via upload

--- UTILS/BIT_MATH.h
+++ UTILS/BIT_MATH.h
@@ -8,9 +8,9 @@
 #ifndef UTILS_BIT_MATH_H_
 #define UTILS_BIT_MATH_H_
 
-#define SET_BIT(byte,bit) byte|=(0x01<<bit)
-#define CLR_BIT(byte,bit) byte&= ~(0x01<<bit)
-#define TOG_BIT(byte,bit) byte^=(0x01<<bit)
+#define SET_BIT(byte,bit) (byte)|=(0x01<<bit)
+#define CLR_BIT(byte,bit) (byte)&= ~(0x01<<bit)
+#define TOG_BIT(byte,bit) (byte)^=(0x01<<bit)
 #define GET_BIT(byte,bit) (byte>>bit)&0x01
 
 #endif /* UTILS_BIT_MATH_H_ */
